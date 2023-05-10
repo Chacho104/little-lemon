53f9109e22e81a9bbe01bd61ecf35d48ac398052
Churchil Owino <chachowino75@gmail.com>
2023-05-10 16:32:16 +0300

    Create the hero cmp

---
+++ src/components/home-page/Hero.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import classes from "./Hero.module.scss";
+import Button from "../ui/Button";
+
+const Hero: React.FC = () => {
+  return (
+    <main className={classes["hero-section"]}>
+      <section className={classes.hero}>
+        <div className={classes.content}>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <Button link="/reservations">Reserve a Table</Button>
+        </div>
+        <div className={classes.image}>
+          <img src="/images/restauranfood.jpg" alt="Little Lemon Chef" />
+        </div>
+      </section>
+    </main>
+  );
+};
+export default Hero;
