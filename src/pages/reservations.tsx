f450cda75bfb718e6fbc10e12c1b5d022c0efbd5
Churchil Owino <chachowino75@gmail.com>
2023-05-11 23:06:08 +0300

    Create reservations page cmp

---
+++ src/pages/reservations.tsx
@@ -0,0 +1,7 @@
+import Reservations from "@/components/reservations-page/Reservations";
+import React from "react";
+
+const reservations: React.FC = () => {
+  return <Reservations />;
+};
+export default reservations;
