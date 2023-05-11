2a5284296cc1205e0023f925228a13ef1e7722a8
Churchil Owino <chachowino75@gmail.com>
2023-05-11 23:05:49 +0300

    Create login page boilterplate

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import AuthPageBg from "@/components/ui/AuthPageBg";
+import React from "react";
+
+const login: React.FC = () => {
+  return (
+    <AuthPageBg>
+      <p>This is the Log In Page...</p>
+    </AuthPageBg>
+  );
+};
+export default login;
