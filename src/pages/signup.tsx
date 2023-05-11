2d973c96807a8a70fc503da3c5040658afe11b84
Churchil Owino <chachowino75@gmail.com>
2023-05-11 23:06:20 +0300

    Create sign up page boilterplate

---
+++ src/pages/signup.tsx
@@ -0,0 +1,11 @@
+import AuthPageBg from "@/components/ui/AuthPageBg";
+import React from "react";
+
+const signup: React.FC = () => {
+  return (
+    <AuthPageBg>
+      <p>This is the Sign Up Page...</p>
+    </AuthPageBg>
+  );
+};
+export default signup;
