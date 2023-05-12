fc78d14279e93a01ff4864ae7e53e96ce2339c85
Churchil Owino <chachowino75@gmail.com>
2023-05-12 14:19:29 +0300

    Add validation and errror messafe cmp

--- src/components/reservations-page/StepOne.tsx
+++ src/components/reservations-page/StepOne.tsx
@@ -1,5 +1,5 @@
-import { Field, Form, Formik } from "formik";
-import React, { MouseEventHandler } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React from "react";
 import { BsPerson } from "react-icons/bs";
 import { FaGlassCheers } from "react-icons/fa";
 import { HiOutlineCalendar } from "react-icons/hi";
@@ -7,6 +7,7 @@ import classes from "./StepOne.module.scss";
 import TimeOptions from "./TimeOptions";
 import Button from "../ui/Button";
 import Image from "next/image";
+import * as Yup from "yup";
 
 type StepOneProps = {
   next: any;
@@ -35,12 +36,33 @@ const StepOne: React.FC<StepOneProps> = ({
           time,
           seatingArea,
         }}
+        validationSchema={Yup.object({
+          date: Yup.date()
+            .min(
+              new Date(Date.now() - 24 * 60 * 60 * 1000).toLocaleString("en", {
+                timeZone: "EAT",
+                hour12: false,
+              })
+            )
+            .required("Please enter a valid date")
+            .label("Date"),
+          noOfDiners: Yup.string().required("Please select number of guests"),
+          occasion: Yup.string()
+            .oneOf(
+              ["Birthday", "Engagement", "Anniversary", "Other"],
+              "Invalid Occassion"
+            )
+            .required("Please select an occassion"),
+          time: Yup.string().required("Please select time for the booking"),
+          seatingArea: Yup.string().required("Please select seating area"),
+        })}
         onSubmit={(values) => {
           next(values);
         }}
       >
         <Form>
           <div className={classes["form-input"]}>
+            <h1>Reserve a table</h1>
             <div className={classes.controls}>
               <div className={classes.control}>
                 <label htmlFor="date">Date</label>
@@ -48,6 +70,9 @@ const StepOne: React.FC<StepOneProps> = ({
                   <HiOutlineCalendar />
                   <Field id="date" name="date" type="date" />
                 </div>
+                <ErrorMessage name="date">
+                  {(msg) => <div className={classes.error}>{msg}</div>}
+                </ErrorMessage>
               </div>
               <div className={classes.control}>
                 <label htmlFor="diners">Number of Diners</label>
@@ -55,18 +80,21 @@ const StepOne: React.FC<StepOneProps> = ({
                   <BsPerson />
                   <Field id="diners" name="noOfDiners" as="select">
                     <option value="">No. of Diners</option>
-                    <option value="1">1 Diner</option>
-                    <option value="2">2 Diners</option>
-                    <option value="3">3 Diners</option>
-                    <option value="4">4 Diners</option>
-                    <option value="5">5 Diners</option>
-                    <option value="6">6 Diners</option>
-                    <option value="7">7 Diners</option>
-                    <option value="8">8 Diners</option>
-                    <option value="9">9 Diners</option>
-                    <option value="10">10 Diners</option>
+                    <option value="1 Diner">1 Diner</option>
+                    <option value="2 Diners">2 Diners</option>
+                    <option value="3 Diners">3 Diners</option>
+                    <option value="4 Diners">4 Diners</option>
+                    <option value="5 Diners">5 Diners</option>
+                    <option value="6 Diners">6 Diners</option>
+                    <option value="7 Diners">7 Diners</option>
+                    <option value="8 Diners">8 Diners</option>
+                    <option value="9 Diners">9 Diners</option>
+                    <option value="10 Diners">10 Diners</option>
                   </Field>
                 </div>
+                <ErrorMessage name="noOfDiners">
+                  {(msg) => <div className={classes.error}>{msg}</div>}
+                </ErrorMessage>
               </div>
             </div>
             <div className={classes.controls}>
@@ -76,12 +104,15 @@ const StepOne: React.FC<StepOneProps> = ({
                   <FaGlassCheers />
                   <Field id="occasion" name="occasion" as="select">
                     <option value="">Occasion</option>
-                    <option value="birthday">Birthday</option>
-                    <option value="engagement">Engagement</option>
-                    <option value="anniversary">Anniversary</option>
-                    <option value="other">Other</option>
+                    <option value="Birthday">Birthday</option>
+                    <option value="Engagement">Engagement</option>
+                    <option value="Anniversary">Anniversary</option>
+                    <option value="Other">Other</option>
                   </Field>
                 </div>
+                <ErrorMessage name="occasion">
+                  {(msg) => <div className={classes.error}>{msg}</div>}
+                </ErrorMessage>
               </div>
               <TimeOptions />
             </div>
@@ -107,6 +138,9 @@ const StepOne: React.FC<StepOneProps> = ({
                   Outdoor seating
                 </label>
               </div>
+              <ErrorMessage name="seatingArea">
+                {(msg) => <div className={classes.error}>{msg}</div>}
+              </ErrorMessage>
             </div>
           </div>
           <div className={classes.action}>
@@ -138,7 +172,7 @@ const StepOne: React.FC<StepOneProps> = ({
                 </div>
               </div>
               <div className={classes.act}>
-                <Button>Reserve a Table</Button>
+                <Button type="submit">Reserve a Table</Button>
               </div>
             </div>
           </div>
