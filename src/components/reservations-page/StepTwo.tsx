b8d3529b0260ae9dd0f8552a54aa0d840a9db935
Churchil Owino <chachowino75@gmail.com>
2023-05-15 22:00:14 +0300

    Remove props destructuring

--- src/components/reservations-page/StepTwo.tsx
+++ src/components/reservations-page/StepTwo.tsx
@@ -23,19 +23,7 @@ type StepTwoProps = {
   phone: string;
 };
 
-const StepTwo: React.FC<StepTwoProps> = ({
-  next,
-  prev,
-  date,
-  noOfDiners,
-  occasion,
-  time,
-  seatingArea,
-  firstName,
-  lastName,
-  email,
-  phone,
-}) => {
+const StepTwo: React.FC<StepTwoProps> = (props) => {
   const phoneRegExp =
     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
@@ -43,10 +31,15 @@ const StepTwo: React.FC<StepTwoProps> = ({
     <>
       <Formik
         initialValues={{
-          firstName,
-          lastName,
-          email,
-          phone,
+          date: props.date,
+          noOfDiners: props.noOfDiners,
+          occasion: props.occasion,
+          time: props.time,
+          seatingArea: props.seatingArea,
+          firstName: props.firstName,
+          lastName: props.lastName,
+          email: props.email,
+          phone: props.phone,
         }}
         validationSchema={Yup.object({
           firstName: Yup.string()
@@ -65,143 +58,150 @@ const StepTwo: React.FC<StepTwoProps> = ({
             .required("Please enter your phone number"),
         })}
         onSubmit={(values) => {
-          next(values, true);
+          props.next(values, true);
         }}
       >
-        <Form>
-          <div className={classes["form-input"]}>
-            <div className={classes["prev-data"]}>
-              <div className={classes.data}>
-                <HiOutlineCalendar />
-                <p>{date}</p>
-              </div>
-              <div className={classes.data}>
-                <BsPerson />
-                <p>{noOfDiners}</p>
-              </div>
-            </div>
-            <div className={classes["prev-data"]}>
-              <div className={classes.data}>
-                <FaGlassCheers />
-                <p>{occasion}</p>
-              </div>
-              <div className={classes.data}>
-                <GiAlarmClock />
-                <p>{time} Hours</p>
-              </div>
-            </div>
-            <div className={classes["prev-data"]}>
-              <div className={classes.data}>
-                <GiTabletopPlayers />
-                <p>{seatingArea}</p>
-              </div>
-              <div className={classes.data}></div>
-            </div>
-            <div className={classes.controls}>
-              <div className={classes.control}>
-                <label htmlFor="first-name">First Name</label>
-                <div className={classes.input}>
-                  <Field
-                    id="first-name"
-                    name="firstName"
-                    placeholder="First name"
-                  />
+        {({ values }) => (
+          <Form>
+            <div className={classes["form-input"]}>
+              <div className={classes["prev-data"]}>
+                <div className={classes.data}>
+                  <HiOutlineCalendar />
+                  <p>{props.date}</p>
                 </div>
-                <ErrorMessage name="firstName">
-                  {(msg) => <div className={classes.error}>{msg}</div>}
-                </ErrorMessage>
-              </div>
-              <div className={classes.control}>
-                <label htmlFor="last-name">Last Name</label>
-                <div className={classes.input}>
-                  <Field
-                    id="last-name"
-                    name="lastName"
-                    placeholder="Last name"
-                  />
-                </div>
-                <ErrorMessage name="lastName">
-                  {(msg) => <div className={classes.error}>{msg}</div>}
-                </ErrorMessage>
-              </div>
-            </div>
-            <div className={classes.controls}>
-              <div className={classes.control}>
-                <label htmlFor="email">Email</label>
-                <div className={classes.input}>
-                  <Field
-                    id="email"
-                    name="email"
-                    placeholder="you@company.com"
-                  />
-                </div>
-                <ErrorMessage name="email">
-                  {(msg) => <div className={classes.error}>{msg}</div>}
-                </ErrorMessage>
-              </div>
-              <div className={classes.control}>
-                <label htmlFor="phone">Phone Number</label>
-                <div className={classes.input}>
-                  <Field id="phone" name="phone" placeholder="Phone number" />
-                </div>
-                <ErrorMessage name="phone">
-                  {(msg) => <div className={classes.error}>{msg}</div>}
-                </ErrorMessage>
-              </div>
-            </div>
-            <div className={classes["radio-group"]}>
-              <div id="my-radio-group" className={classes.label}>
-                User Agreement
-              </div>
-              <div role="group" aria-labelledby="my-radio-group">
-                <label>
-                  <Field
-                    type="radio"
-                    name="seatingArea"
-                    value="Indoor seating"
-                  />
-                  You agree to our friendly privacy policy
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className={classes.action}>
-            <div className={classes.content}>
-              <div className={classes.images}>
-                <div>
-                  <Image
-                    src="/images/chicago2.jpg"
-                    width={200}
-                    height={190}
-                    alt="Little lemon restaurant"
-                  />
-                </div>
-                <div>
-                  <Image
-                    src="/images/owners2.jpg"
-                    width={200}
-                    height={190}
-                    alt="Little lemon chefs"
-                  />
-                </div>
-                <div>
-                  <Image
-                    src="/images/bruchetta.jpg"
-                    width={200}
-                    height={190}
-                    alt="Little lemon food"
-                  />
+                <div className={classes.data}>
+                  <BsPerson />
+                  <p>{props.noOfDiners}</p>
                 </div>
               </div>
-              <div className={classes.act}>
-                <Button type="button" onClick={() => prev()}>
-                  Edit Reservattion
-                </Button>
-                <Button type="submit">Confirm Reservattion</Button>
+              <div className={classes["prev-data"]}>
+                <div className={classes.data}>
+                  <FaGlassCheers />
+                  <p>{props.occasion}</p>
+                </div>
+                <div className={classes.data}>
+                  <GiAlarmClock />
+                  <p>{props.time} Hours</p>
+                </div>
+              </div>
+              <div className={classes["prev-data"]}>
+                <div className={classes.data}>
+                  <GiTabletopPlayers />
+                  <p>{props.seatingArea}</p>
+                </div>
+                <div className={classes.data}></div>
+              </div>
+              <div className={classes.controls}>
+                <div className={classes.control}>
+                  <label htmlFor="first-name">First Name</label>
+                  <div className={classes.input}>
+                    <Field
+                      id="first-name"
+                      name="firstName"
+                      placeholder="First name"
+                    />
+                  </div>
+                  <ErrorMessage name="firstName">
+                    {(msg) => <div className={classes.error}>{msg}</div>}
+                  </ErrorMessage>
+                </div>
+                <div className={classes.control}>
+                  <label htmlFor="last-name">Last Name</label>
+                  <div className={classes.input}>
+                    <Field
+                      id="last-name"
+                      name="lastName"
+                      placeholder="Last name"
+                    />
+                  </div>
+                  <ErrorMessage name="lastName">
+                    {(msg) => <div className={classes.error}>{msg}</div>}
+                  </ErrorMessage>
+                </div>
+              </div>
+              <div className={classes.controls}>
+                <div className={classes.control}>
+                  <label htmlFor="email">Email</label>
+                  <div className={classes.input}>
+                    <Field
+                      id="email"
+                      name="email"
+                      placeholder="you@company.com"
+                    />
+                  </div>
+                  <ErrorMessage name="email">
+                    {(msg) => <div className={classes.error}>{msg}</div>}
+                  </ErrorMessage>
+                </div>
+                <div className={classes.control}>
+                  <label htmlFor="phone">Phone Number</label>
+                  <div className={classes.input}>
+                    <Field id="phone" name="phone" placeholder="Phone number" />
+                  </div>
+                  <ErrorMessage name="phone">
+                    {(msg) => <div className={classes.error}>{msg}</div>}
+                  </ErrorMessage>
+                </div>
+              </div>
+              <div className={classes["radio-group"]}>
+                <div id="my-radio-group" className={classes.label}>
+                  User Agreement
+                </div>
+                <div role="group" aria-labelledby="my-radio-group">
+                  <label>
+                    <Field
+                      type="radio"
+                      name="seatingArea"
+                      value="Indoor seating"
+                    />
+                    You agree to our friendly privacy policy
+                  </label>
+                </div>
               </div>
             </div>
-          </div>
-        </Form>
+            <div className={classes.action}>
+              <div className={classes.content}>
+                <div className={classes.images}>
+                  <div>
+                    <Image
+                      src="/images/site/chicago2.jpg"
+                      width={200}
+                      height={190}
+                      alt="Little lemon restaurant"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      src="/images/site/owners2.jpg"
+                      width={200}
+                      height={190}
+                      alt="Little lemon chefs"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      src="/images/site/bruchetta.jpg"
+                      width={200}
+                      height={190}
+                      alt="Little lemon food"
+                    />
+                  </div>
+                </div>
+                <div className={classes.act}>
+                  <Button
+                    type="button"
+                    onClick={() => {
+                      props.prev(values);
+                    }}
+                  >
+                    Edit Reservation
+                  </Button>
+                  <Button type="submit">Confirm Reservation</Button>
+                </div>
+              </div>
+            </div>
+          </Form>
+        )}
       </Formik>
     </>
   );
