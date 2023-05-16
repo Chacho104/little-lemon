d816299d5c37ebb82ea86f0239d74dca7ffab468
Churchil Owino <chachowino75@gmail.com>
2023-05-16 07:58:16 +0300

    Rename the about page cmp

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,12 +2,12 @@ import About from "@/components/about-page/About";
 import PageHero from "@/components/ui/PageHero";
 import React from "react";
 
-const about: React.FC = () => {
+const AboutPage = () => {
   return (
     <>
-      <PageHero image="/images/owners1.jpg" alt="Mario and Adrian" />
+      <PageHero image="/images/site/owners1.jpg" alt="Mario and Adrian" />
       <About />
     </>
   );
 };
-export default about;
+export default AboutPage;
