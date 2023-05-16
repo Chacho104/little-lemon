3be50e52c1ec5f1af76f4d10f39f86c21f23ade9
Churchil Owino <chachowino75@gmail.com>
2023-05-16 07:55:15 +0300

    Rename the login page cmp

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,11 @@
 import AuthPageBg from "@/components/ui/AuthPageBg";
 import React from "react";
 
-const login: React.FC = () => {
+const LoginPage = () => {
   return (
     <AuthPageBg>
       <p>This is the Log In Page...</p>
     </AuthPageBg>
   );
 };
-export default login;
+export default LoginPage;
