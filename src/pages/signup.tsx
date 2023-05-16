fbf44839e51ef2fa0c6b941f6d2744338b9edbc0
Churchil Owino <chachowino75@gmail.com>
2023-05-16 07:54:51 +0300

    Rename the page cmp

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,11 +1,11 @@
 import AuthPageBg from "@/components/ui/AuthPageBg";
 import React from "react";
 
-const signup: React.FC = () => {
+const SignupPage = () => {
   return (
     <AuthPageBg>
       <p>This is the Sign Up Page...</p>
     </AuthPageBg>
   );
 };
-export default signup;
+export default SignupPage;
