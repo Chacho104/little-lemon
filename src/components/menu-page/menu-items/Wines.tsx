034efdbe953bf46177a62616fafaa1bc14a32815
Churchil Owino <chachowino75@gmail.com>
2023-05-19 17:04:42 +0300

    Create wines cmp

--- src/components/menu-page/menu-items/Wines.tsx
+++ src/components/menu-page/menu-items/Wines.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import MenuItemsList from "../menu-markup/MenuItemsList";
 
 type WinesProps = {
-  meals: any[];
+  meals: {
+    _id: string;
+    category: string;
+    title: string;
+    price: number;
+    isFeatured: boolean;
+    description: string;
+    slug: string;
+    image: string;
+  }[];
 };
 
 const Wines: React.FC<WinesProps> = ({ meals }) => {
