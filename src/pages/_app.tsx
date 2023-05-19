4d3a60cd9b46fbd7913711c770355e4d62d109aa
Churchil Owino <chachowino75@gmail.com>
2023-05-19 16:51:31 +0300

    Provide the redux store to the entire app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "@/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
