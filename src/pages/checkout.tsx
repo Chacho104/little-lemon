28e3f90efc87ad538059db309e6e2d9492839d79
Churchil Owino <chachowino75@gmail.com>
2023-05-19 16:51:07 +0300

    Create the checkout page cmp

---
+++ src/pages/checkout.tsx
@@ -0,0 +1,7 @@
+import Checkout from "@/components/checkout-page/Checkout";
+import React from "react";
+
+const CheckoutPage: React.FC = () => {
+  return <Checkout />;
+};
+export default CheckoutPage;
