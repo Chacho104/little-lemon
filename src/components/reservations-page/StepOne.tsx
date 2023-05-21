510ad075918b705d879f3828742cc62ad22ad3ae
Churchil Owino <chachowino75@gmail.com>
2023-05-21 13:09:24 +0300

    Refactor component to use custom input cmps

--- src/components/reservations-page/StepOne.tsx
+++ src/components/reservations-page/StepOne.tsx
@@ -1,4 +1,4 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import React from "react";
 import { BsPerson } from "react-icons/bs";
 import { FaGlassCheers } from "react-icons/fa";
@@ -8,6 +8,8 @@ import TimeOptions from "./TimeOptions";
 import Button from "../ui/Button";
 import Image from "next/image";
 import * as Yup from "yup";
+import CustomInput from "../inputs/CustomInput";
+import RadioCustomInput from "../inputs/RadioCustomInput";
 
 type StepOneProps = {
   next: any;
@@ -20,9 +22,31 @@ type StepOneProps = {
   lastName: string;
   email: string;
   phone: string;
+  userAgreement: string;
 };
 
 const StepOne: React.FC<StepOneProps> = (props) => {
+  const dateLabel = (
+    <div className={classes["custom-label"]}>
+      <HiOutlineCalendar />
+      <span>Date</span>
+    </div>
+  );
+
+  const dinersLabel = (
+    <div className={classes["custom-label"]}>
+      <BsPerson />
+      <span>Number of Diners</span>
+    </div>
+  );
+
+  const occasionLabel = (
+    <div className={classes["custom-label"]}>
+      <FaGlassCheers />
+      <span>Occasion</span>
+    </div>
+  );
+
   const bookingDetailsForm = (
     <>
       <Formik
@@ -36,6 +60,7 @@ const StepOne: React.FC<StepOneProps> = (props) => {
           lastName: props.lastName,
           email: props.email,
           phone: props.email,
+          userAgreement: props.userAgreement,
         }}
         validationSchema={Yup.object({
           date: Yup.date()
@@ -66,56 +91,44 @@ const StepOne: React.FC<StepOneProps> = (props) => {
             <div className={classes["form-input"]}>
               <h1>Reserve a table</h1>
               <div className={classes.controls}>
-                <div className={classes.control}>
-                  <label htmlFor="date">Date</label>
-                  <div className={classes.input}>
-                    <HiOutlineCalendar />
-                    <Field id="date" name="date" type="date" />
-                  </div>
-                  <ErrorMessage name="date">
-                    {(msg) => <div className={classes.error}>{msg}</div>}
-                  </ErrorMessage>
-                </div>
-                <div className={classes.control}>
-                  <label htmlFor="diners">Number of Diners</label>
-                  <div className={classes.input}>
-                    <BsPerson />
-                    <Field id="diners" name="noOfDiners" as="select">
-                      <option value="">No. of Diners</option>
-                      <option value="1 Diner">1 Diner</option>
-                      <option value="2 Diners">2 Diners</option>
-                      <option value="3 Diners">3 Diners</option>
-                      <option value="4 Diners">4 Diners</option>
-                      <option value="5 Diners">5 Diners</option>
-                      <option value="6 Diners">6 Diners</option>
-                      <option value="7 Diners">7 Diners</option>
-                      <option value="8 Diners">8 Diners</option>
-                      <option value="9 Diners">9 Diners</option>
-                      <option value="10 Diners">10 Diners</option>
-                    </Field>
-                  </div>
-                  <ErrorMessage name="noOfDiners">
-                    {(msg) => <div className={classes.error}>{msg}</div>}
-                  </ErrorMessage>
-                </div>
+                <CustomInput
+                  name="date"
+                  label={dateLabel}
+                  id="date"
+                  type="date"
+                />
+                <CustomInput
+                  label={dinersLabel}
+                  name="noOfDiners"
+                  id="diners"
+                  as="select"
+                >
+                  <option value="">No. of Diners</option>
+                  <option value="1 Diner">1 Diner</option>
+                  <option value="2 Diners">2 Diners</option>
+                  <option value="3 Diners">3 Diners</option>
+                  <option value="4 Diners">4 Diners</option>
+                  <option value="5 Diners">5 Diners</option>
+                  <option value="6 Diners">6 Diners</option>
+                  <option value="7 Diners">7 Diners</option>
+                  <option value="8 Diners">8 Diners</option>
+                  <option value="9 Diners">9 Diners</option>
+                  <option value="10 Diners">10 Diners</option>
+                </CustomInput>
               </div>
               <div className={classes.controls}>
-                <div className={classes.control}>
-                  <label htmlFor="occasion">Occasion</label>
-                  <div className={classes.input}>
-                    <FaGlassCheers />
-                    <Field id="occasion" name="occasion" as="select">
-                      <option value="">Occasion</option>
-                      <option value="Birthday">Birthday</option>
-                      <option value="Engagement">Engagement</option>
-                      <option value="Anniversary">Anniversary</option>
-                      <option value="Other">Other</option>
-                    </Field>
-                  </div>
-                  <ErrorMessage name="occasion">
-                    {(msg) => <div className={classes.error}>{msg}</div>}
-                  </ErrorMessage>
-                </div>
+                <CustomInput
+                  label={occasionLabel}
+                  name="occasion"
+                  id="occasion"
+                  as="select"
+                >
+                  <option value="">Occasion</option>
+                  <option value="Birthday">Birthday</option>
+                  <option value="Engagement">Engagement</option>
+                  <option value="Anniversary">Anniversary</option>
+                  <option value="Other">Other</option>
+                </CustomInput>
                 <TimeOptions />
               </div>
               <div className={classes["radio-group"]}>
@@ -123,22 +136,16 @@ const StepOne: React.FC<StepOneProps> = (props) => {
                   Seating Area
                 </div>
                 <div role="group" aria-labelledby="my-radio-group">
-                  <label>
-                    <Field
-                      type="radio"
-                      name="seatingArea"
-                      value="Indoor seating"
-                    />
-                    Indoor seating
-                  </label>
-                  <label>
-                    <Field
-                      type="radio"
-                      name="seatingArea"
-                      value="Outdoor seating"
-                    />
-                    Outdoor seating
-                  </label>
+                  <RadioCustomInput
+                    name="seatingArea"
+                    children="Indoor seating"
+                    value="Indoor seating"
+                  />
+                  <RadioCustomInput
+                    name="seatingArea"
+                    children="Outdoor seating"
+                    value="Outdoor seating"
+                  />
                 </div>
                 <ErrorMessage name="seatingArea">
                   {(msg) => <div className={classes.error}>{msg}</div>}
