a8d98f006530132767a0b5a3ae98ab50613c9908
Churchil Owino <chachowino75@gmail.com>
2023-05-22 17:24:43 +0300

    Add head cmp and meta data

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,7 +1,19 @@
 import Checkout from "@/components/checkout-page/Checkout";
+import Head from "next/head";
 import React from "react";
 
 const CheckoutPage: React.FC = () => {
-  return <Checkout />;
+  return (
+    <>
+      <Head>
+        <title>Order Online</title>
+        <meta
+          name="description"
+          content="Place an order for your favorite food or drink and we will deliver swiftly to your doorstep!"
+        />
+      </Head>
+      <Checkout />
+    </>
+  );
 };
 export default CheckoutPage;
