fe018da097f7ef193ad121a7e7861b48e1d73144
Churchil Owino <chachowino75@gmail.com>
2023-05-22 17:37:00 +0300

    Delete unneeded import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Inter } from "next/font/google";
 import Hero from "@/components/home-page/Hero";
 import AboutBrief from "@/components/home-page/AboutBrief";
 import MenuHighlight from "@/components/home-page/MenuHighlight";
