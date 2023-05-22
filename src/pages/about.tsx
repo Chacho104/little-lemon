f9b030d46af35a49ea993f320b29fe944087441d
Churchil Owino <chachowino75@gmail.com>
2023-05-22 17:24:53 +0300

    Add head cmp and meta data

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,18 @@
 import About from "@/components/about-page/About";
 import PageHero from "@/components/ui/PageHero";
+import Head from "next/head";
 import React from "react";
 
 const AboutPage = () => {
   return (
     <>
+      <Head>
+        <title>About Little Lemon</title>
+        <meta
+          name="description"
+          content="Meet the Little Lemon team and find out what's makes us the preferred restaurant of many locals!"
+        />
+      </Head>
       <PageHero image="/images/site/owners1.jpg" alt="Mario and Adrian" />
       <About />
     </>
