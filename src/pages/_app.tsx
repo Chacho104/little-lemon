71a85cd9a4e3e9889ef0e1ca8a8deb46ac31ccdb
Churchil Owino <chachowino75@gmail.com>
2023-05-22 17:25:04 +0300

    Add head cmp and meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,24 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Layout>
+        <Head>
+          <title>Little Lemon Restaurant</title>
+          <link rel="icon" href="/favicon.png" />
+          <meta
+            name="description"
+            content="Welcome to Little Lemon, a mediterranean restaurant serving traditional recipes with a modern twist!"
+          />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </Provider>
