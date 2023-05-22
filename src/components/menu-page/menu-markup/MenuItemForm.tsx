a468c87c70baaad0ae0d430f3552ca32c0c94bba
Churchil Owino <chachowino75@gmail.com>
2023-05-22 17:25:54 +0300

    Remove extras addition from menu item form

--- src/components/menu-page/menu-markup/MenuItemForm.tsx
+++ src/components/menu-page/menu-markup/MenuItemForm.tsx
@@ -35,33 +35,6 @@ const MenuItemForm: React.FC<MenuItemFormProps> = ({ id, onAddToCart }) => {
           defaultValue="1"
         />
       </div>
-      <div>
-        <h2>Extras</h2>
-      </div>
-      <div className={classes.add}>
-        <div>
-          <label htmlFor="feta">Add feta</label>
-        </div>
-        <div>
-          <input type="checkbox" id="feta" name="feta" />
-        </div>
-      </div>
-      <div className={classes.add}>
-        <div>
-          <label htmlFor="parmesan">Add parmesan</label>
-        </div>
-        <div>
-          <input type="checkbox" id="parmesan" name="parmesan" />
-        </div>
-      </div>
-      <div className={classes.add}>
-        <div>
-          <label htmlFor="dressing">Add dressing</label>
-        </div>
-        <div>
-          <input type="checkbox" id="dressing" name="dressing" />
-        </div>
-      </div>
       <div className={classes.act}>
         <Button variant="formbtn" type="submit">
           Add to Basket
