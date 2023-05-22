c84f808c8756badc764099f60f868ebb0c956115
Churchil Owino <chachowino75@gmail.com>
2023-05-22 17:23:44 +0300

    Add head cmp and meta data

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,21 @@
 import AuthPageBg from "@/components/ui/AuthPageBg";
+import Head from "next/head";
 import React from "react";
 
 const LoginPage = () => {
   return (
-    <AuthPageBg>
-      <p>This is the Log In Page...</p>
-    </AuthPageBg>
+    <>
+      <Head>
+        <title>Little Lemon Log In</title>
+        <meta
+          name="description"
+          content="Log in to your Little Lemon account to continue enjoying personalized services!"
+        />
+      </Head>
+      <AuthPageBg>
+        <p>This is the Log In Page...</p>
+      </AuthPageBg>
+    </>
   );
 };
 export default LoginPage;
