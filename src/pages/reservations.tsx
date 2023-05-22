43e637a94bc0a82b234fe1aa949f2fab696f6e70
Churchil Owino <chachowino75@gmail.com>
2023-05-22 17:23:38 +0300

    Add head cmp and meta data

--- src/pages/reservations.tsx
+++ src/pages/reservations.tsx
@@ -1,7 +1,19 @@
 import Reservations from "@/components/reservations-page/Reservations";
+import Head from "next/head";
 import React from "react";
 
 const ReservationsPage = () => {
-  return <Reservations />;
+  return (
+    <>
+      <Head>
+        <title>Reserve a Table</title>
+        <meta
+          name="description"
+          content="Find and reserve a table for any occasion at Little Lemon!"
+        />
+      </Head>
+      <Reservations />
+    </>
+  );
 };
 export default ReservationsPage;
